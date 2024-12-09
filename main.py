87612ba6ba8bd0a58b139941f34713989b239a41
Aquil-ouadah <aquil.ouadah@edu.esiee.fr>
2024-12-09 13:06:49 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 #### Fonction secondaire
 
 
-def ispalindrome(p):
+#### Fonction secondaire
+"""Ce code permet de vérifier si une chaîne de caractère est un palindrome"""
 
-    # votre code ici
-    
-    return False
+def ispalindrome(s):
+    """Fonction qui vérifie si une chaîne de caractère s est un palindrome"""
+    s = s.lower()
+
+    changement_de_var = str.maketrans('éèêëàâäîïôöùûüç', 'eeeeaaaiioouuuc', ' ",?;.:/!\'-')
+    s = s.translate(changement_de_var)
+    s_inv = s[::-1]
+    return s == s_inv
 
 #### Fonction principale
 
 
 def main():
-
-    # vos appels à la fonction secondaire ici
+    """Cette fonction est la fonction principale"""
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
